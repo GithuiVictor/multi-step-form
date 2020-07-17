3bf58877c07f37fe1aea23ed63a122694e2a7e00
GithuiVictor <victormaina23@gmail.com>
2020-07-17 11:36:51 +0300

    Updates

---
+++ precache-manifest.0f8b68b9658202b55447a1c217f55aa1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d487d3796182c0d97f17f86b7f817d04",
+    "url": "/multi-step-form/index.html"
+  },
+  {
+    "revision": "863bc4b0d18cf293d1b1",
+    "url": "/multi-step-form/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d70506575dcf75aa87f6",
+    "url": "/multi-step-form/static/js/2.31ffb7dc.chunk.js"
+  },
+  {
+    "revision": "971b4e562d3abe5fe0995509ffca7edb",
+    "url": "/multi-step-form/static/js/2.31ffb7dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "863bc4b0d18cf293d1b1",
+    "url": "/multi-step-form/static/js/main.c685575a.chunk.js"
+  },
+  {
+    "revision": "99f04d5736a607a8ca42",
+    "url": "/multi-step-form/static/js/runtime-main.8896c669.js"
+  }
+]);
